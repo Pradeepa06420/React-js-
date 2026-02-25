2d036252f3d3fd2625c326fccaa51edf06707264
Pradeepa06420 <pradee062826@gmail.com>
2026-02-25 21:16:52 +0530

    Create App.jsx

---
+++ Program/Color picker/App.jsx
@@ -0,0 +1,15 @@
+import React,{useState} from 'react'
+
+function App() {
+  const[color,setColor]=useState("white")
+  return (
+    <div>
+      <h1>Color picker</h1>
+      <button onClick={()=>setColor("red")}>Red</button>
+      <button onClick={()=>setColor("green")}>Green</button>
+      <button onClick={()=>setColor("blue")}>Blue</button>       
+      <div className='bgchange' style={{background:color}}></div>
+    </div>
+  )
+}
+export default App;
